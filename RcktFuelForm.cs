900cd8383759f82cea61cac93c9ffe54df7be744
Shannon <shannoncareer@outlook.com>
2019-04-12 08:09:14 -0500

    NASA Rocket Fuel Software
    C# Windows Form Application
    How much propellant is needed to launch NASA's Terrier-Malemute Rocket into various level of Earth's atmosphere.

---
+++ RcktFuelForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RcktFuel
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+        InitializeComponent();
+        }
+
+        private void ButtonFuelCalc_Click(object sender, EventArgs e){
+            bool troposphereSelected = true; 
+            double TroposphereMilesMax = 14500;  //max miles of troposphere
+            double terriMalMass = 3260; //unit = lbs
+
+            if (troposphereSelected){ 
+                double terriMalGallons = TroposphereMilesMax * terriMalMass;               
+                labelFuelage.Text = Convert.ToString(terriMalGallons);
+            }
+            else
+            {
+            Console.WriteLine("Please make a selection");
+            }
+                
+            }
+
+        private void ComboBoxRocketType_SelectedIndexChanged(object sender, EventArgs e){
+            
+            object TerMalRocket;
+               //Weight of Rocket 
+            
+
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
