71c70b62abdbbb304d52c4d53d9f7f66f7e0d8ab
Shannon <shannoncareer@outlook.com>
2019-04-12 06:47:39 -0500

    Prompt Function
    C# Console Application 
    User input w/return of string input

--- Program.cs
+++ Program.cs
@@ -4,37 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LBA_staff2019
+namespace toprmp
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.Title = "The LBA Group Staff 2019";
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Prompt("Please type your name: ");
+            Console.Clear();
+            Console.WriteLine("The name entered was: " + name);
+            Console.ReadKey();
+        }//end main
 
-			string[] jobs = new string[7] {"Sales","Finance","HR","PR","Custodial","CRM","iT"};
+        static string Prompt(string msg)
+        {
+            Console.WriteLine(msg);
+            return Console.ReadLine();
+        }
+    }//end class
 
-			Console.WriteLine("Until the company grows, you're going to have to do all of these jobs: ");
-
-				for(int x = 0; x < jobs.Length; x++)
-				{
-				Console.WriteLine(jobs[x]); 
-				} //end for loop 
-
-			Console.Write("\n");
-
-			Console.WriteLine("Using the foreach loop, it will display as: ");
-
-			foreach (string roles in jobs)
-			{
-				Console.WriteLine(roles);
-			}
-			Console.Write("\n");
-
-			Console.WriteLine("Follow us at @TheLBAGroup = Instagram ");
-			Console.WriteLine("Facebook = www.facebook.com/thelbagroup.co ");
-
-			Console.ReadKey();
-		}
-	}
-}
+   
+    
+}//end namespace
